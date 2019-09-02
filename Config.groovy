ccbc985909f16e2429fd6e03818c9113ab29a320
Arturs Drozdovs <arturs.drozdovs@teampicnic.com>
2019-09-02 23:11:37 +0200

    add docs

--- Config.groovy
+++ Config.groovy
@@ -1,4 +1,3 @@
-
 // Path where the docToolchain will produce the output files.
 // This path is appended to the docDir property specified in gradle.properties
 // or in the command line, and therefore must be relative to it.
@@ -7,19 +6,18 @@ outputPath = 'docs'
 inputPath = '.'
 
 inputFiles = [
-        [file: 'arc42-template.adoc', formats: ['html','pdf','docbook']],
-        [file: 'ppt/Demo.pptx.ad', formats: ['revealjs']]
-             ]
+        [file: 'arc42-template.adoc', formats: ['html', 'pdf', 'docbook']]
+]
 
 taskInputsDirs = ["${inputPath}/src",
                   "${inputPath}/images",
-                 ]
+]
 
 taskInputsFiles = ["${inputPath}/arc42-template.adoc"]
 
 confluence = [:]
 confluence.with {
-    input = [[ file: "build/" ]]
+    input = [[file: "build/"]]
     api = '<your API>'
     spaceKey = 'arc42'
     createSubpages = false
