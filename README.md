ce1b23ace905e1661619c129841e2359bbdfb86f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-24 13:22:09 +0000

    Deploy WillemGerritzen/testing-docs to github.com/WillemGerritzen/testing-docs.git:gh-pages

---
+++ README.md
@@ -0,0 +1,4 @@
+# testing-docs
+1. clone repo
+2. change something in src dir
+3. commit push
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+./docToolchain/bin/doctoolchain ./src generateHTML
+mv src/docs/html5/* .
+rm -rf src docToolchain
